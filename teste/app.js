562d5f0f6180a19a38cf0ecde36ba5dbfb3381dd
ANDREI DO ESPÍRITO SANTO FIUZA <24122362@CFP127ALUNO.LOCAL>
2025-04-17 11:25:00 -0300

    arrumando os repositorios do git

--- teste/app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const likeButton = document.querySelector(".like-button");
-    const heartIcon = likeButton.querySelector(".heart-icon");
-
-    likeButton.addEventListener("click", function () {
-        heartIcon.classList.toggle("liked");
-    });
-});
